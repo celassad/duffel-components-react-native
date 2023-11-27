ae5c45570526f54eed2294b9f1d0309cf4cd1f87
Chaimaa <chaimaa@cglacet.local>
2023-11-27 15:51:22 +0100

    Add translation library

--- src/Components/DuffelAncillaries/BaggageSelectionCard.tsx
+++ src/Components/DuffelAncillaries/BaggageSelectionCard.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import {
   Modal,
   ScrollView,
@@ -8,11 +9,26 @@ import {
   View,
 } from 'react-native';
 import { Icon, Text } from 'react-native-elements';
+import {
+  Offer,
+  OfferSlice,
+  OfferSliceSegmentPassengerBaggage,
+  Passenger,
+  Service,
+  ServiceMetadata,
+} from '../../duffelTypes';
 
 const TITLE = 'Extra baggage';
 const SUBTITLE = 'Add any extra baggage you need for your trip';
 
-export default function BaggageSelectionCard() {
+export default function BaggageSelectionCard({
+  offer,
+  passengers,
+}: {
+  offer: Offer;
+  passengers: Passenger[];
+}) {
+  const { t } = useTranslation();
   const [visible, setVisible] = useState(false);
 
   function handleModal() {
@@ -27,11 +43,17 @@ export default function BaggageSelectionCard() {
       <TouchableOpacity style={styles.container} onPress={showModal}>
         <Icon name="luggage" style={styles.iconStyle} />
         <View>
+          <Text>{t('welcome')}</Text>
           <Text style={styles.titleStyle}>{TITLE}</Text>
           <Text style={styles.subtitleStyle}>{SUBTITLE}</Text>
         </View>
       </TouchableOpacity>
-      <BaggageSelectionModal handleModal={handleModal} visible={visible} />
+      <BaggageSelectionModal
+        offer={offer}
+        passengers={passengers}
+        handleModal={handleModal}
+        visible={visible}
+      />
     </>
   );
 }
@@ -39,9 +61,13 @@ export default function BaggageSelectionCard() {
 function BaggageSelectionModal({
   handleModal,
   visible,
+  offer,
+  passengers,
 }: {
   handleModal: () => void;
   visible: boolean;
+  offer: Offer;
+  passengers: Passenger[];
 }) {
   return (
     <Modal
@@ -64,7 +90,7 @@ function BaggageSelectionModal({
                 style={{ height: 350, paddingBottom: 10 }}
                 keyboardShouldPersistTaps="always"
               >
-                <BaggageSelectionView />
+                <BaggageSelectionView offer={offer} passengers={passengers} />
               </ScrollView>
             </View>
           </TouchableWithoutFeedback>
@@ -74,8 +100,241 @@ function BaggageSelectionModal({
   );
 }
 
-function BaggageSelectionView() {
-  return <Text>test</Text>;
+function BaggageSelectionView({
+  offer,
+  passengers,
+}: {
+  offer: Offer;
+  passengers: Passenger[];
+}) {
+  return (
+    <SliceBaggageSelection
+      offer={offer}
+      slice={offer.slices?.[0]}
+      passengers={passengers}
+    />
+  );
+}
+function SliceBaggageSelection({
+  offer,
+  slice,
+  passengers,
+}: {
+  offer: Offer;
+  slice: OfferSlice;
+  passengers: Passenger[];
+}) {
+  const title = `Vol de ${slice.origin.iata_city_code} à ${slice.destination.iata_city_code}`;
+
+  return (
+    <View>
+      <Text style={styles.sliceTitle}>{title}</Text>
+      {passengers?.map((p, i) => {
+        return (
+          <PassengerBagage
+            passenger={p}
+            index={i}
+            slice={slice}
+            offer={offer}
+          />
+        );
+      })}
+    </View>
+  );
+}
+
+function PassengerBagage({
+  passenger,
+  index,
+  slice,
+  offer,
+}: {
+  passenger: Passenger;
+  index: number;
+  slice: OfferSlice;
+  offer: Offer;
+}) {
+  const includedBaggage = slice?.segments?.[0]?.passengers?.[index]?.baggages;
+  const hasIncludedBaggage = includedBaggage?.reduce(
+    (sum, bag) => sum + bag.quantity,
+    0
+  );
+  const passengerServicesForSegment = offer?.available_services?.filter(
+    ({ type, passenger_ids, segment_ids }) =>
+      type === 'baggage' &&
+      passenger_ids.includes(passenger.id) &&
+      segment_ids.includes(slice?.segments?.[0]?.id)
+  ) as Service[];
+
+  return (
+    <View>
+      <Text
+        style={styles.passengerName}
+      >{`${passenger.given_name} ${passenger.family_name}`}</Text>
+      {hasIncludedBaggage ? (
+        <IncludedBaggageBanner includedBaggage={includedBaggage} />
+      ) : null}
+
+      {passengerServicesForSegment.map((availableService) => (
+        <BaggageSelectionController
+          key={availableService.id}
+          passengerId={passenger.id}
+          segmentId={slice.segments?.[0]?.id}
+          availableService={availableService}
+          // selectedServices={selectedServices}
+          // quantity={
+          //     selectedServices.find(
+          //         ({ id }) => id == availableService.id
+          //     )?.quantity || 0
+          // }
+          // onQuantityChanged={(newQuantity) =>
+          //     onBaggageQuantityChanged(
+          //         newQuantity,
+          //         segmentId,
+          //         passengerId,
+          //         passengerName,
+          //         availableService,
+          //         selectedServices,
+          //         setSelectedServices
+          //     )
+          // }
+        />
+      ))}
+      {passengerServicesForSegment.length === 0 && (
+        <Text style={styles.addBagageNotAvailableText}>
+          Aucun bagage supplémentaire disponible pour ce passager
+        </Text>
+      )}
+    </View>
+  );
+}
+
+export const getBaggageServiceDescription = (metadata: ServiceMetadata) => {
+  if (!metadata) {
+    return null;
+  }
+
+  if (
+    !metadata.maximum_weight_kg &&
+    !metadata.maximum_length_cm &&
+    !metadata.maximum_height_cm &&
+    !metadata.maximum_depth_cm
+  ) {
+    return null;
+  }
+
+  const hasAllDimensions =
+    metadata.maximum_length_cm &&
+    metadata.maximum_height_cm &&
+    metadata.maximum_depth_cm;
+
+  let dimensionsLabel = '';
+  if (hasAllDimensions) {
+    dimensionsLabel = ` / ${metadata.maximum_height_cm} x ${metadata.maximum_length_cm} x ${metadata.maximum_depth_cm} cm`;
+  }
+
+  let weightLabel = '';
+  if (metadata.maximum_weight_kg) {
+    weightLabel = `Up to ${metadata.maximum_weight_kg}kg`;
+  }
+
+  return `${weightLabel}${dimensionsLabel}`;
+};
+
+function BaggageSelectionController({
+  availableService,
+  passengerId,
+  segmentId,
+}: {
+  availableService: Service;
+  passengerId: string;
+  segmentId: string;
+}) {
+  const serviceName =
+    availableService.metadata.type === 'carry_on' ? 'Cabin bag' : 'Checked bag';
+
+  const servicePrice = `${availableService.total_amount} ${availableService.total_currency}`;
+
+  const serviceDescription = getBaggageServiceDescription(
+    availableService.metadata
+  );
+
+  //   const shouldDisableController =
+  //     hasServiceOfSameMetadataTypeAlreadyBeenSelected(
+  //       selectedServices,
+  //       segmentId,
+  //       passengerId,
+  //       availableService
+  //     );
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-start',
+      }}
+    >
+      <View>
+        <Text>{serviceName}</Text>
+        <Text>{servicePrice}</Text>
+        <Text>{serviceDescription}</Text>
+      </View>
+      <Counter />
+    </View>
+  );
+}
+
+function Counter() {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <Icon name="remove" containerStyle={{ borderWidth: 1 }} />
+
+      <Text style={{ padding: 5 }}>0</Text>
+      <Icon name="add" containerStyle={{ borderWidth: 1 }} />
+    </View>
+  );
+}
+
+function IncludedBaggageBanner({
+  includedBaggage,
+}: {
+  includedBaggage: OfferSliceSegmentPassengerBaggage[];
+}) {
+  const { carryOnBagsQuantity, checkedBagsQuantity } = includedBaggage.reduce(
+    (sum, { type, quantity }) => ({
+      carryOnBagsQuantity:
+        sum.carryOnBagsQuantity + (type === 'carry_on' ? quantity : 0),
+      checkedBagsQuantity:
+        sum.checkedBagsQuantity + (type === 'checked' ? quantity : 0),
+    }),
+    {
+      carryOnBagsQuantity: 0,
+      checkedBagsQuantity: 0,
+    }
+  );
+
+  const baggageLabelStringArray = [];
+  if (carryOnBagsQuantity > 0) {
+    baggageLabelStringArray.push(
+      withPlural(carryOnBagsQuantity, 'bagage cabine', 'bagages cabine')
+    );
+  }
+  if (checkedBagsQuantity > 0) {
+    baggageLabelStringArray.push(
+      withPlural(checkedBagsQuantity, 'bagage en soute', 'bagages en soute')
+    );
+  }
+  return (
+    <View style={styles.includedBagBannerView}>
+      <Text style={styles.includedBagText}>
+        {baggageLabelStringArray.join(' et ')} inclus
+      </Text>
+    </View>
+  );
+}
+
+export function withPlural(quantity: number, singular: string, plural: string) {
+  return quantity > 1 ? `${quantity} ${plural}` : `${quantity} ${singular}`;
 }
 
 const BORDER_RADIUS = 20;
@@ -89,14 +348,13 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
   },
   iconStyle: {
-    paddingHorizontal: 10,
+    paddingRight: 10,
   },
   container: {
     borderRadius: 10,
     borderWidth: 1,
     padding: 15,
     flexDirection: 'row',
-    margin: 10,
     borderColor: '#E2E2E8',
   },
   overlayStyle: {
@@ -109,5 +367,35 @@ const styles = StyleSheet.create({
     backgroundColor: 'rgba(0,0,0, 0.2)',
     flex: 1,
     justifyContent: 'flex-end',
+  },
+  addBagageNotAvailableText: {
+    color: '#74747C',
+    fontWeight: 'bold',
+    padding: 5,
+    fontSize: 14,
+  },
+  passengerName: {
+    fontWeight: 'bold',
+    color: 'hsl(203, 14%, 23%)',
+    padding: 10,
+    fontSize: 16,
+  },
+  includedBagText: {
+    color: '#064E3B',
+  },
+  includedBagBannerView: {
+    margin: 5,
+    padding: 10,
+    backgroundColor: '#ECFDF5',
+    borderRadius: 10,
+  },
+  sliceTitle: {
+    fontWeight: 'bold',
+    color: 'hsl(203, 14%, 23%)',
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  containerStyle: {
+    margin: 20,
   },
 });
